c849abe86a4b809333402057e6c55cc81660d5ab
seanyzhang <seanz2005@gmail.com>
2025-06-26 21:27:13 -0400

    Time: 19 ms (80.78%), Space: 16.1 MB (65.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -6,8 +6,7 @@ class Solution(object):
         """
         d = defaultdict(list)
         for word in strs:
-            c = Counter(word)
-            key = frozenset(c.items())
+            key = "".join(sorted(word))
             d[key].append(word)
         
         return list(d.values())
